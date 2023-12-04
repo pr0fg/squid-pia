24db41c67a5ca7bc79f4758b6c6108968c42e0d7
Garrett Hayes <garrett@Garretts-Mac-mini.local>
2023-12-04 10:55:40 -0500

    Fixed restart bug that caused squid to fail

--- squid-pia/squid/start.sh
+++ squid-pia/squid/start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-echo "[INFO] Starting squid daemon..." | ts '%Y-%m-%d %H:%M:%.S'
-tail -vn 0 -F /var/log/squid/access.log &
-squid -N -d 1
+if ! pgrep squid; then
+	echo "[INFO] Starting squid daemon..." | ts '%Y-%m-%d %H:%M:%.S'
+	tail -vn 0 -F /var/log/squid/access.log &
+	squid -N -d 1
+fi
